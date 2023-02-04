478987d87b912b990a3e3d64e1e161e5ae2ded71
alexia rodriguez <alexia.rodriguez11211@gmail.com>
2023-02-04 17:36:25 -0600

    Update

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+function createNewStudent(req: Request, res: Response): void {
+  
+    const studentData = // Assign `req.body` as a `NewStudentRequest`
+  
+    const didAddStudent = // Call the `addStudent` function using the student's data
+  
+    // If the student's data was not added successfully
+      // Responds with status 409 (This means 409 Conflict)
+      // return from the function
+  
+    // Send status 201 (This means 201 Created)
+  }
+
+  
