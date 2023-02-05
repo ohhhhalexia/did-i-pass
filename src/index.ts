ecce37e3fc79c96d85e4e93e5b607fabc1697683
alexia rodriguez <alexia.rodriguez11211@gmail.com>
2023-02-05 07:26:55 -0600

    progress on index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@
 import express, { Express } from 'express';
 import { notImplemented } from './controllers/NotImplementedController';
 
-
 // Import functions from StudentController
+import StudentController from './controllers/StudentController';
+
 
 // Create your app object
 const app: Express = express();
@@ -13,11 +14,11 @@ const PORT = 8091;
 // Activate json parsing for the request body
 
 // Register your route handlers for the specified endpoints
-app.post('/api/students', notImplemented); //create a new student 
+app.post('/api/students', StudentController.students); //create a new student 
 app.get ('/api/students/:studentName' , notImplemented); //Get a student's data
 app.get ('/api/students/:studentName}/finalExam', notImplemented); // Get the score the student needs on their final exam to receive the corresponding letter grade.
 app.post ('/api/students/:studentName/finalExam', notImplemented); //Calculate what the student's overall score would be if they got the specified grade on the final exam.
-app.post ('/api/students/:studentName/grades/:assignmentName'); //Update a student's grade for the corresponding grade name.
+app.post ('/api/students/:studentName/grades/:assignmentName', notImplemented); //Update a student's grade for the corresponding grade name.
 
 // Start listening on the chosen port
 app.listen(PORT, () => {
