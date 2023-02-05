362e4d039cc5aea3daba52b12eff99e2305e2e1a
alexia rodriguez <alexia.rodriguez11211@gmail.com>
2023-02-04 21:32:10 -0600

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 // Import express
 import express, { Express } from 'express';
 
+post /api/students //create a new student 
+
+get /api/students/{studentName} //Get a student's data
+
+get /api/students/{studentName}/finalExam //Get the score the student needs on their final exam to receive the corresponding letter grade.
+
+post /api/students/{studentName}/finalExam //Calculate what the student's overall score would be if they got the specified grade on the final exam.
+
+post /api/students/{studentName}/grades/{assignmentName} //Update a student's grade for the corresponding grade name.
 
 // Import functions from StudentController
 
