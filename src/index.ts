bc3fb09133de31060d2dcfe5ae1d782ffe1377e3
alexia rodriguez <alexia.rodriguez11211@gmail.com>
2023-02-04 22:07:21 -0600

    completed index.ts according to 2nd video

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,7 @@
 // Import express
 import express, { Express } from 'express';
+import { notImplemented } from './controllers/NotImplementedController';
 
-post /api/students //create a new student 
-
-get /api/students/{studentName} //Get a student's data
-
-get /api/students/{studentName}/finalExam //Get the score the student needs on their final exam to receive the corresponding letter grade.
-
-post /api/students/{studentName}/finalExam //Calculate what the student's overall score would be if they got the specified grade on the final exam.
-
-post /api/students/{studentName}/grades/{assignmentName} //Update a student's grade for the corresponding grade name.
 
 // Import functions from StudentController
 
@@ -21,6 +13,11 @@ const PORT = 8091;
 // Activate json parsing for the request body
 
 // Register your route handlers for the specified endpoints
+app.post('/api/students', notImplemented); //create a new student 
+app.get ('/api/students/:studentName' , notImplemented); //Get a student's data
+app.get ('/api/students/:studentName}/finalExam', notImplemented); // Get the score the student needs on their final exam to receive the corresponding letter grade.
+app.post ('/api/students/:studentName/finalExam', notImplemented); //Calculate what the student's overall score would be if they got the specified grade on the final exam.
+app.post ('/api/students/:studentName/grades/:assignmentName'); //Update a student's grade for the corresponding grade name.
 
 // Start listening on the chosen port
 app.listen(PORT, () => {
